878c3ce505dd4adf59b5e08940d9879980037ebb
gopal221296 <gopalakrishnan.v@unacademy.com>
2019-07-12 10:57:04 +0530

    Allow Methods

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ models.sequelize
   });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   next();
 });
 
